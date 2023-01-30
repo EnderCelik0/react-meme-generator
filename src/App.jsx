a9699b6aed30bef0c7b0c90bf566e02b06ba25c8
Ender Çelik <64286011+EnderCelik0@users.noreply.github.com>
2023-01-30 15:38:37 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './components/Navbar';
+import Meme from './components/Meme';
+import './App.css';
+
+
+export default function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Meme />
+    </div>
+  );
+}
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import ok from '../img/okEmoji.png';
+
+export default function Header() {
+  return (
+    <header className='header'>
+      <img src={ok} className='header--image' />
+      <h2 className='header--title'>Meme Generator</h2>
+      <h4 className='header--project'>React Course - Project 3</h4>
+    </header>
+  );
+}
